179964cde301639661c0c667e9bfbaeccaaf5491
Юля <vanda2722@gmail.com>
2021-11-25 20:12:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Вы на ветке develop. Добро пожаловать!
+        </p>
+        <p>
+
+
+          Редактируйте <code>src/App.js</code> и сохраните для обновления.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Изучаем реакт!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
